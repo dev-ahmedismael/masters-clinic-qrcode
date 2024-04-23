1bdcc35afb82c26cffa588572d6ac1b3064f7bec
Ahmed Ismael <dev.ahmedismael@outlook.com>
2024-04-23 15:58:38 +0200

    First Release!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { QrCodeModule } from 'ng-qrcode';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, QrCodeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mastersclinicqr';
+  constructor(private fb: FormBuilder) {}
+  title = 'Masters Clinic QR Code';
+
+  // QR Code Form
+  qr_form = this.fb.group({
+    url: [''],
+  });
+
+  get url() {
+    return this.qr_form.controls['url'];
+  }
+
+  generate_qr() {
+    this.qrcode_value = this.qr_form.controls.url.value;
+    this.qr_form.reset();
+  }
+
+  // QR Code
+  qrcode_value: any = '';
 }
